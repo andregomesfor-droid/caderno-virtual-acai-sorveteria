c422d94c195cfb38722214656d69c913015ef856
Andre Gomes <andregomes.for@gmail.com>
2026-09-15 11:44:42 -0300

    Corrigido erro de cores nos botoes

--- caderno-virtual-acai-sorveteria-main/app.js
+++ caderno-virtual-acai-sorveteria-main/app.js
@@ -95,7 +95,6 @@ function carregarProdutos() {
     btnEditar.textContent = "Editar";
     btnEditar.className = "text-btn";
     btnEditar.style.marginRight = "10px";
-    btnEditar.style.color = "#0066cc";
     btnEditar.style.background = "none";
     btnEditar.style.border = "none";
     btnEditar.style.cursor = "pointer";
@@ -104,7 +103,6 @@ function carregarProdutos() {
     const btnExcluir = document.createElement("button");
     btnExcluir.textContent = "Excluir";
     btnExcluir.className = "text-btn";
-    btnExcluir.style.color = "#cc0000";
     btnExcluir.style.background = "none";
     btnExcluir.style.border = "none";
     btnExcluir.style.cursor = "pointer";
